e7f55533cb5220ec42871a32ff8e24f0a8474657
Thomas Lohnert <thomas.lohnert@stfc.ac.uk>
2018-02-28 18:28:00 +0000

    parse input file into matrix

--- TestProblem.py
+++ TestProblem.py
@@ -4,10 +4,25 @@ class PizzaSlicer:
         self.f = open(file_path)
         first_line = self.f.readline()
         params = first_line.split(" ")
-        self.rows = params[0]
-        self.cols = params[1]
-        self.min_ingredients = params[2]
-        self.max_slice = params[3]
+        self.rows = int(params[0])
+        self.cols = int(params[1])
+        self.min_ingredients = int(params[2])
+        self.max_slice = int(params[3])
+        self.model = self.parse_file()
+
+    def parse_file(self):
+        lines = self.f.readlines()
+        matrix = [[0 for x in range(self.cols)] for y in range(self.rows)]
+        i = 0
+        for line in lines:
+            j = 0
+            for letter in line:
+                if letter == "T":
+                    matrix[i][j] = 1
+                j += 1
+
+            i += 1
+        return matrix
 
 slicer = PizzaSlicer("example.in")
 print("rows {}".format(slicer.rows))
