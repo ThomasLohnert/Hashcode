e32edd3663049d3ce15e7727cc3efcd8453d342a
Thomas Lohnert <thomas.lohnert@stfc.ac.uk>
2018-02-28 19:01:51 +0000

    parse as numpy array

--- example_problem/TestProblem.py
+++ example_problem/TestProblem.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Slice:
     def __init__(self, start_row, start_col, end_row, end_col):
         self.start_row = start_row
@@ -12,11 +14,17 @@ class PizzaSlicer:
         self.f = open(file_path)
         first_line = self.f.readline()
         params = first_line.split(" ")
+
         self.rows = int(params[0])
         self.cols = int(params[1])
         self.min_ingredients = int(params[2])
         self.max_slice = int(params[3])
-        self.model = self.parse_file()
+
+        self.model = np.array(self.parse_file())
+
+        self.num_m = len(self.model[self.model == 0])
+        self.num_t = len(self.model[self.model == 1])
+        
         self.f.close()
 
     def parse_file(self):
