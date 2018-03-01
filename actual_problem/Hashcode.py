be1d10f58bd03500181943d4485dcb4b6038026e
Thomas Lohnert <thomas.lohnert@stfc.ac.uk>
2018-03-01 19:54:08 +0000

    track vehicles

--- actual_problem/Hashcode.py
+++ actual_problem/Hashcode.py
@@ -16,17 +16,19 @@ class RideScheduler:
         self.vehicle_list = [Vehicle(i) for i in range(self.num_v)]
 
     def assign_jobs(self):
+
+        for v in self.vehicle_list:
+            if len(self.rides) == 0:
+                break
+            r = self.rides.pop()
+            v.update(r)
+
         while len(self.rides):
             for v in self.vehicle_list:
                 if len(self.rides) == 0:
                     break
                 r = self.rides.pop()
-                v.jobs.append(r)
+                v.update(r)
 
         return self.vehicle_list
 
-    def calc_distance(self, start, end):
-        return abs(start[0]-end[0]) + abs(start[1]-end[0])
-
-    def order_by_start(self,):
-        pass
--- actual_problem/Vehicle.py
+++ actual_problem/Vehicle.py
@@ -2,6 +2,17 @@ class Vehicle:
     def __init__(self, id):
         self.id = id
         self.jobs = []
+        self.time = 0
+        self.last_pos = (0, 0)
 
     def move(self):
         pass
+
+    def update(self, ride):
+        self.jobs.append(ride)
+        self.time = self.time + self.calc_distance(self.last_pos, ride.start_pos) \
+                    + self.calc_distance(ride.start_pos, ride.end_pos)
+        self.last_pos = ride.end_pos
+
+    def calc_distance(self, start, end):
+        return abs(start[0] - end[0]) + abs(start[1] - end[0])
