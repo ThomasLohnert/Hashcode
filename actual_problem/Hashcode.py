5e35e3a84b091904360565c5e3bd10e51a23fef8
Thomas Lohnert <thomas.lohnert@stfc.ac.uk>
2018-03-01 17:58:18 +0000

    what we got

--- actual_problem/Hashcode.py
+++ actual_problem/Hashcode.py
@@ -1,18 +1,27 @@
 import numpy as np
 import matplotlib
 
-class SomeClass:
+class Ride:
+    def __init__(self, start_pos, end_pos, start_time, end_time ):
+        self.start_pos =
+
+class RideScheduler:
 
     def __init__(self, file_path):
         self.f = open(file_path)
         first_line = self.f.readline()
-        a, b, c, d = first_line.split(" ")
+        self.rows, self.cols, self.vehicles, self.rides = first_line.split(" ")
 
         self.cut(self.model)
         print(self.slices)
         self.f.close()
 
-    def write_file(self, slices):
-        out = open("output.txt", "w+")
+    def write_result(self):
+        out = open("schedule.txt", "w+")
 
         out.close()
+
+    def calc_distance(self, start, end):
+        return abs(start[0]-end[0]) + abs(start[1]-end[0])
+
+    def order_by_start(self,):
---
+++ actual_problem/HashcodeTest.py
@@ -0,0 +1,9 @@
+import unittest
+from Hashcode import RideScheduler
+
+class RideSchedulerTest(unittest.TestCase):
+
+    def set_up(self):
+        scheduler = RideScheduler()
+
+    def test
