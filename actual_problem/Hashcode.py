5c124f5a38c45c55866c3d6c70bf0e920987549b
Thomas Lohnert <thomas.lohnert@stfc.ac.uk>
2018-03-01 17:31:55 +0000

    basic io

--- actual_problem/Hashcode.py
+++ actual_problem/Hashcode.py
@@ -1,1 +1,18 @@
-print("hi")
+import numpy as np
+import matplotlib
+
+class SomeClass:
+
+    def __init__(self, file_path):
+        self.f = open(file_path)
+        first_line = self.f.readline()
+        a, b, c, d = first_line.split(" ")
+
+        self.cut(self.model)
+        print(self.slices)
+        self.f.close()
+
+    def write_file(self, slices):
+        out = open("output.txt", "w+")
+
+        out.close()
