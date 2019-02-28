b30781c0a2417df875f9d314e38f4734f98b4aba
Samuel Jackson <samueljackson@outlook.com>
2019-02-28 17:59:20 +0000

    Add file input code

--- 2019/file_io.py
+++ 2019/file_io.py
@@ -1,12 +1,15 @@
+import os
 
 def read_input(file_name):
     """
     Reads in an input file and returns:
     """
-    with open(file_name) as f:
-        _, _, _ = f.readline().split()
-
-        return
+    with open(os.path.join("data/", file_name)) as f:
+        lines = f.readlines()
+        lines = lines[1:]
+        line_parts = [line.split() for line in lines]
+        line_parts = [(parts[1], parts[0], parts[2:]) for parts in line_parts]
+        return line_parts
 
 
 def write_output(file_name):
