032c51fe7f88cdcd4c98c7b38426ff1801c6daad
Dominic Oram <dominic.oram@stfc.ac.uk>
2019-02-28 19:43:39 +0000

    added real func

--- 2019/pairing.py
+++ 2019/pairing.py
@@ -1,8 +1,8 @@
-def pair(photos):
+def pair(photos, averageH):
     """
     Takes a list of vertical photos and returns a paired list
     :param photos: list[tuple] : [(idn, orientation, num_of_tags, tags)]
-    :return: list[(),()] [(idn, orientation, num_of_tags, tags), (idn, orientation, num_of_tags, tags)]
+    :return: list[((),())] [((idn, orientation, num_of_tags, tags), (idn, orientation, num_of_tags, tags))]
     """
     out = []
     last_vertical = None
@@ -14,3 +14,11 @@ def pair(photos):
             last_vertical = None
 
     return out
+
+def findHAverage(horiz_photos):
+    total = 0
+    for p in horiz_photos:
+        total +=p[2]
+    return total/len(horiz_photos)
+
+def
