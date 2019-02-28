e53be5ff437a5ef264af81f2fc1e9c90bda1c031
Dominic Oram <dominic.oram@stfc.ac.uk>
2019-02-28 20:13:56 +0000

    Fixed typo

--- 2019/pairing.py
+++ 2019/pairing.py
@@ -23,7 +23,7 @@ def pair(vert_photos, averageH):
 def pair_single(p1,p2):
     #((id1,id2),v,tags
     tags = list(set().union(p1[3], p2[3]))
-    return ((p1[0], p2[1]), "V", len(tags), tags)
+    return ((p1[0], p2[0]), "V", len(tags), tags)
 
 def findHAverage(horiz_photos):
     total = 0
